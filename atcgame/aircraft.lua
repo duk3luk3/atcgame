8c660b550f8e811c784474b3cb21bb2c865933a5
Lukas Erlacher <erlacher@in.tum.de>
2014-12-23 23:53:38 +0100

    align captions

--- atcgame/aircraft.lua
+++ atcgame/aircraft.lua
@@ -167,7 +167,7 @@ function m:draw(scene)
     if self.s.log[self.s.log.first] then
       local cmd = self.s.log[self.s.log.first]
       love.graphics.print(
-      string.format("HDG %3.f (%s)", self.s.heading, cmd['name'] or string.format("%03d", cmd['heading'] or self.s.set_heading)),
+      string.format("%03.f (%s)", self.s.heading, cmd['name'] or string.format("%03d", cmd['heading'] or self.s.set_heading)),
       p.x + 5, p.y + 17)
 
       if cmd['for'] then
@@ -178,17 +178,21 @@ function m:draw(scene)
       end
     else
       love.graphics.print(
-      string.format("HDG %3.f (%3.f)", self.s.heading, self.s.set_heading),
+      string.format("%03.f (%03.f)", self.s.heading, self.s.set_heading),
       p.x + 5, p.y + 17)
     end
 
 
     love.graphics.print(
-    string.format("SPD %.f (%.f)", self.s.speed, self.s.set_speed),
+    string.format("%.f (%.f)", self.s.speed, self.s.set_speed),
     p.x + 5, p.y + 29)
     love.graphics.print(
-    string.format("ALT %.f (%.f)", self.s.pos.z, self.s.set_altitude),
+    string.format("%.f (%.f)", self.s.pos.z, self.s.set_altitude),
     p.x + 5, p.y + 41)
+
+		love.graphics.print("HDG", p.x-25, p.y+17)
+		love.graphics.print("SPD", p.x-25, p.y+29)
+		love.graphics.print("ALT", p.x-25, p.y+41)
 
     if self.s.target then
       love.graphics.print(self.s.target['name'], p.x + 5, p.y - 15)
